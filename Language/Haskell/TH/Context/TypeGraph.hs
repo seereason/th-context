d2f1ffeb146e88b06aa3495249842bbb907c5df7
David Fox <dsf@seereason.com>
2015-05-04 05:08:20 -0700

    Add the TypeGraphInfo record and the functions to build it

--- Language/Haskell/TH/Context/TypeGraph.hs
+++ Language/Haskell/TH/Context/TypeGraph.hs
@@ -13,6 +13,11 @@ module Language.Haskell.TH.Context.TypeGraph
     , TypeGraphVertex(..)
     , typeVertex
     , fieldVertex
+    , TypeGraphInfo
+    , hints, typeSet, edges, expanded
+    , typeVertices
+    , typeNames
+    , typeGraphInfo
     , TypeGraphEdges
     , typeGraphEdges
     , typeGraphVertices
@@ -27,14 +32,18 @@ module Language.Haskell.TH.Context.TypeGraph
 import Control.Applicative
 import Data.Monoid (mempty)
 #endif
+import Control.Lens (makeLenses, view)
+import Control.Monad.Reader (MonadReader)
 import Control.Monad.State (execStateT, modify, MonadState(get), StateT)
 import Control.Monad.Trans (lift)
 import Data.Default (Default(def))
 import Data.Generics (Data, everywhere, mkT)
 import Data.Graph (Graph, Vertex, graphFromEdges)
 import Data.List as List (concatMap, intersperse, map)
-import Data.Map as Map (Map, filter, fromList, fromListWith, keys, lookup, map, mapKeys, toList, update, alter)
-import Data.Set as Set (empty, fromList, insert, map, member, null, Set, toList, union)
+import Data.Map as Map (Map, filter, findWithDefault, fromList, fromListWith, insert, insertWith,
+                        keys, lookup, map, mapKeys, toList, update, alter)
+import Data.Maybe (mapMaybe)
+import Data.Set as Set (empty, fromList, insert, map, member, null, Set, singleton, toList, union)
 import Language.Haskell.Exts.Syntax ()
 import Language.Haskell.TH -- (Con, Dec, nameBase, Type)
 import Language.Haskell.TH.Context.Expand (E(E), expandType, runExpanded)
@@ -113,6 +122,177 @@ data VertexHint
 instance Default VertexHint where
     def = Normal
 
+-- | Information collected about the graph implied by the structure of
+-- one or more 'Type' values.
+data TypeGraphInfo
+    = TypeGraphInfo
+      { _typeSet :: Set (E Type)
+      -- All the types encountered, including embedded types such as the
+      -- 'Maybe' and the 'Int' in @Maybe Int@.
+      , _expanded :: Map Type (E Type)
+      -- The types with all type synonyms replaced with their expansions.
+      , _synonyms :: Map (E Type) (Set Name)
+      -- The names of all the type synonyms which expand to a type
+      , _fields :: Map (E Type) (Set (Name, Name, Either Int Name))
+      -- Information about record fields which contain a given type
+      , _edges :: Map (E Type) (Set (E Type))
+      -- The edges of the "has this subtype" graph.  A type has
+      -- subtypes via either type application ('AppT'), field type
+      -- ('StrictType', 'VarStrictType') or the 'VertexHint'
+      -- mechanism.
+      , _hints :: Map TypeGraphVertex [VertexHint]
+      }
+
+$(makeLenses ''TypeGraphInfo)
+
+-- | Find all the 'TypeGraphVertex' that involve this type.  All
+-- returned nodes will have the same set of type synonyms, but there
+-- will be one for each field where the type appears and one with
+-- field Nothing.
+typeVertices :: MonadReader TypeGraphInfo m => E Type -> m (Set TypeGraphVertex)
+typeVertices typ = do
+  syns <- view synonyms >>= return . Map.findWithDefault Set.empty typ
+  flds <- view fields >>= return . Set.insert Nothing . Set.map Just . Map.findWithDefault Set.empty typ
+  return $ Set.map (\ f -> TypeGraphVertex {_etype = typ, _syns = syns, _field = f}) flds
+
+-- | Return the set of 'Name' of a type's synonyms, plus the name (if
+-- any) used in its data declaration.  Note that this might return the
+-- empty set.
+typeNames :: TypeGraphVertex -> Set Name
+typeNames (TypeGraphVertex {_etype = E (ConT tname), _syns = s}) = Set.insert tname s
+typeNames (TypeGraphVertex {_syns = s}) = s
+
+-- | This is now the only function that actually recurses through the
+-- type structure.  It collects the set of all accessable types.
+scanTypes :: forall m. DsMonad m => [Type] -> m (Set Type)
+scanTypes types =
+    execStateT (mapM doType types) mempty
+    where
+      doType :: Type -> StateT (Set Type) m ()
+      doType typ = do
+        (s :: Set Type) <- get
+        case Set.member typ s of
+          True -> return ()
+          False -> modify (Set.insert typ) >> doType' typ
+
+      doType' :: Type -> StateT (Set Type) m ()
+      doType' (ConT name) = qReify name >>= doInfo name
+      doType' (AppT typ1 typ2) = doType typ1 >> doType typ2
+      doType' ListT = return ()
+      doType' (VarT _) = return ()
+      doType' (TupleT _) = return ()
+      doType' typ = error $ "scanTypes: " ++ show typ
+
+      doInfo :: Name -> Info -> StateT (Set Type) m ()
+      doInfo _tname (TyConI dec) = doDec dec
+      doInfo _tname (PrimTyConI _ _ _) = return ()
+      doInfo _ info = error $ "scanTypes: " ++ show info
+
+      doDec :: Dec -> StateT (Set Type) m ()
+      doDec (TySynD _ _ typ) = doType typ
+      doDec (NewtypeD _ _ _ con _) = doCon con
+      doDec (DataD _ _ _ cons _) = mapM_ doCon cons
+      doDec dec = error $ "scanTypes: " ++ pprint dec
+
+      doCon :: Con -> StateT (Set Type) m ()
+      doCon (ForallC _ _ con) = doCon con
+      doCon (NormalC _ flds) = mapM_ doField (zip (List.map Left ([1..] :: [Int])) (List.map snd flds))
+      doCon (RecC _ flds) = mapM_ doField (List.map (\ (fname, _, ftype) -> (Right fname, ftype)) flds)
+      doCon (InfixC (_, lhs) _ (_, rhs)) = mapM_ doField [(Left 1, lhs), (Left 2, rhs)]
+
+      doField :: (Either Int Name, Type) -> StateT (Set Type) m ()
+      doField (_, ftyp) = doType ftyp
+
+findExpanded :: DsMonad m => Set Type -> m (Map Type (E Type))
+findExpanded types =
+    execStateT (mapM (\typ -> expandType typ >>= \etyp -> modify (Map.insert typ etyp)) (Set.toList types)) mempty
+
+findSynonyms :: DsMonad m => Set Type -> m (Map Type (Set Name))
+findSynonyms types =
+    execStateT (mapM_ doType (Set.toList types)) mempty
+    where
+      doType (ConT name) = qReify name >>= doInfo
+      doType (AppT typ1 typ2) = doType typ1 >> doType typ2
+      doType _ = return ()
+      doInfo (TyConI dec) = doDec dec
+      doInfo _ = return ()
+      doDec (TySynD tname _ typ) = modify (Map.insertWith union typ (singleton tname))
+      doDec _ = return ()
+
+findFields :: DsMonad m => Set Type -> m (Map Type (Set (Name, Name, Either Int Name)))
+findFields types =
+    execStateT (mapM_ doType (Set.toList types)) mempty
+    where
+      doType (ConT name) = qReify name >>= doInfo
+      doType (AppT typ1 typ2) = doType typ1 >> doType typ2
+      doType _ = return ()
+
+      doInfo (TyConI dec) = doDec dec
+      doInfo _ = return ()
+
+      doDec (NewtypeD _ tname _ con _) = doCon tname con
+      doDec (DataD _ tname _ cons _) = mapM_ (doCon tname) cons
+      doDec _ = return ()
+
+      doCon tname (ForallC _ _ con) = doCon tname con
+      doCon tname (NormalC cname flds) = mapM_ (doField tname cname) (zip (List.map Left ([1..] :: [Int])) (List.map snd flds))
+      doCon tname (RecC cname flds) = mapM_ (doField tname cname) (List.map (\ (fname, _, ftype) -> (Right fname, ftype)) flds)
+      doCon tname (InfixC (_, lhs) cname (_, rhs)) = mapM_ (doField tname cname) [(Left 1, lhs), (Left 2, rhs)]
+
+      doField tname cname (fname, ftyp) = modify (Map.insertWith union ftyp (singleton (tname, cname, fname)))
+
+findEdges :: DsMonad m => Set Type -> m (Map Type (Set Type))
+findEdges types =
+    execStateT (mapM_ doType (Set.toList types)) mempty
+    where
+      doType (ConT name) = qReify name >>= doInfo
+      doType typ@(AppT typ1 typ2) = modify (Map.insertWith union typ (singleton typ1)) >> modify (Map.insertWith union typ (singleton typ2))
+      doType _ = return ()
+
+      doInfo (TyConI dec) = doDec dec
+      doInfo _ = return ()
+
+      doDec :: (DsMonad m, MonadState (Map Type (Set Type)) m) => Dec -> m ()
+      doDec (TySynD tname _ typ') = modify (Map.insertWith union (ConT tname) (singleton typ'))
+      doDec (NewtypeD _ tname _ con _) = doCon tname con
+      doDec (DataD _ tname _ cons _) = mapM_ (doCon tname) cons
+      doDec _ = return ()
+
+      doCon :: (DsMonad m, MonadState (Map Type (Set Type)) m) => Name -> Con -> m ()
+      doCon tname (ForallC _ _ con) = doCon tname con
+      doCon tname (NormalC _ flds) = mapM_ (doField tname) (List.map snd flds)
+      doCon tname (RecC _ flds) = mapM_ (doField tname) (List.map (\ (_, _, ftype) -> ftype) flds)
+      doCon tname (InfixC (_, lhs) _ (_, rhs)) = mapM_ (doField tname) [lhs, rhs]
+
+      doField :: (DsMonad m, MonadState (Map Type (Set Type)) m) => Name -> Type -> m ()
+      doField tname ftyp = modify (Map.insertWith union (ConT tname) (singleton ftyp))
+
+typeGraphInfo :: forall m. DsMonad m => [(TypeGraphVertex, VertexHint)] -> [Type] -> m TypeGraphInfo
+typeGraphInfo hs types = do
+  let hintTypes = mapMaybe hintType hs
+  types' <- scanTypes (types ++ hintTypes)
+  (ex :: Map Type (E Type)) <- findExpanded types'
+  (sy :: Map (E Type) (Set Name)) <- findSynonyms types' >>= return . Map.fromListWith union . List.map (\ (typ, names) -> let Just etyp = Map.lookup typ ex in (etyp, names))  . Map.toList
+  fl <- findFields types' >>= return . Map.fromListWith union . List.map (\ (typ, names) -> (exptyp ex typ, names))  . Map.toList
+  ed <- findEdges types' >>= return . Map.fromListWith union . List.map (\ (typ, types'') -> (exptyp ex typ, Set.map (exptyp ex) types'')) . Map.toList
+{-
+  hs <- let mp = Map.map sequence (Map.fromListWith (++) (List.map (\ (node, hint) -> (node, [hint])) hints)) in
+        \node -> Map.findWithDefault (return []) node mp
+-}
+  let etypes' = Set.fromList $ List.map (exptyp ex) (Set.toList types')
+  return $ TypeGraphInfo { _expanded = ex
+                         , _synonyms = sy
+                         , _fields = fl
+                         , _typeSet = etypes'
+                         , _edges = ed
+                         , _hints =  Map.fromListWith (++) (List.map (\ (n, h) -> (n, [h])) hs)
+                         }
+      where exptyp ex typ = let Just etyp = Map.lookup typ ex in etyp
+            hintType :: (TypeGraphVertex, VertexHint) -> Maybe Type
+            hintType (_, Divert x) = Just x
+            hintType (_, Extra x) = Just x
+            hintType _ = Nothing
+
 -- | Return the set of types embedded in the given type.  This is just
 -- the nodes of the type graph.  The type synonymes are expanded by the
 -- th-desugar package to make them suitable for use as map keys.
@@ -188,8 +368,8 @@ typeGraphEdges augment types = do
 
                 doCon :: Name -> Dec -> Con -> StateT TypeGraphEdges m ()
                 doCon tname dec (ForallC _ _ con) = doCon tname dec con
-                doCon tname dec (NormalC cname fields) = mapM_ (doField tname dec cname) (zip (List.map Left ([1..] :: [Int])) (List.map snd fields))
-                doCon tname dec (RecC cname fields) = mapM_ (doField tname dec cname) (List.map (\ (fname, _, typ') -> (Right fname, typ')) fields)
+                doCon tname dec (NormalC cname flds) = mapM_ (doField tname dec cname) (zip (List.map Left ([1..] :: [Int])) (List.map snd flds))
+                doCon tname dec (RecC cname flds) = mapM_ (doField tname dec cname) (List.map (\ (fname, _, typ') -> (Right fname, typ')) flds)
                 doCon tname dec (InfixC (_, lhs) cname (_, rhs)) = mapM_ (doField tname dec cname) [(Left 1, lhs), (Left 2, rhs)]
 
                 doField :: Name -> Dec -> Name -> (Either Int Name, Type) -> StateT TypeGraphEdges m ()
